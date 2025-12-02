491db122547ff7ec3c287c2ba3108464f1f92124
marcelschoeckel <marcel.schoeckel@gmail.com>
2025-12-02 22:50:07 +0100

    2025 Day2

---
+++ 2025/src/Day02.kt
@@ -0,0 +1,21 @@
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    println("Day 1!")
+
+    val input = File("resources/day02.txt").readText().trim()
+
+    val IDs = input.split(',')
+        .flatMap {
+            val startEnd = it.split('-').map { it.toLong() }
+            LongRange( startEnd[0], startEnd[1] )
+        }
+        .map { it.toString() }
+
+    val sum1 = IDs.filter { Regex("^(?<digits>\\d+)(\\k<digits>)$").matches(it) }.sumOf { it.toLong() }
+    println("Part 1: $sum1")
+
+    val sum2 = IDs.filter { Regex("^(?<digits>\\d+)(\\k<digits>)+$").matches(it) }.sumOf { it.toLong() }
+    println("Part 2: $sum2")
+}
