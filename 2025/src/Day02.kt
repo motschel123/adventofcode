961074c42c621c3e99b6a5b6f0870a966c3c6b2c
marcelschoeckel <marcel.schoeckel@gmail.com>
2025-12-04 10:35:35 +0100

    Day 2 new layout

--- 2025/src/Day02.kt
+++ 2025/src/Day02.kt
@@ -2,27 +2,37 @@ import java.io.File
 import kotlin.time.measureTime
 
 fun main() {
-    println("Day 2!")
+    fun input(): String {
+        return File("2025/resources/input02.txt").readText().trim()
+            .split(',')
+            .flatMap {
+                val startEnd = it.split('-').map { it.toLong() }
+                LongRange(startEnd[0], startEnd[1])
+            }
+            .joinToString("\n") { it.toString() }
+    }
+
+    fun part1(input: String): String {
+        return Regex("^(?<digits>\\d+)(\\k<digits>)$", RegexOption.MULTILINE).findAll(input).sumOf { it.value.toLong() }.toString()
+    }
+
+    fun part2(input: String): String {
+        return Regex("^(?<digits>\\d+)(\\k<digits>)+$", RegexOption.MULTILINE).findAll(input).sumOf { it.value.toLong() }.toString()
+    }
 
 
-    val input = File("2025/resources/input02.txt").readText().trim()
+    println("Day 02!")
+    val input = input()
 
-    val IDs = input.split(',')
-        .flatMap {
-            val startEnd = it.split('-').map { it.toLong() }
-            LongRange( startEnd[0], startEnd[1] )
-        }
-        .map { it.toString() }
-
-    val sum1: Long
-    println("Time part 1: " + measureTime {
-        sum1 = IDs.filter { Regex("^(?<digits>\\d+)(\\k<digits>)$").matches(it) }.sumOf { it.toLong() }
+    val result1: String
+    println("Part 1 Timing: " + measureTime {
+        result1 = part1(input)
     })
-    println("Part 1: $sum1")
+    println("Part 1 Result: " + result1)
 
-    val sum2: Long
-    println("Time part 2: " + measureTime {
-        sum2 = IDs.filter { Regex("^(?<digits>\\d+)(\\k<digits>)+$").matches(it) }.sumOf { it.toLong() }
+    val result2: String
+    println("Part 2 Timing: " + measureTime {
+        result2 = part2(input)
     })
-    println("Part 2: $sum2")
+    println("Part 2 Result: " + result2)
 }
